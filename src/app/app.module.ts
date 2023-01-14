26ae1b0ed55ea68d85a9325f27257b0738d600b2
Gaston2022 <97002160+Gaston2022@users.noreply.github.com>
2023-01-14 18:25:20 -0300

    se instalo nodeJs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoARComponent } from './components/logo-ar/logo-ar.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoARComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
